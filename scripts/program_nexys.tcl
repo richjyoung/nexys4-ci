125f06564355c0a7387ce117f7542d7a534bab47
Rich Young <rich@build.local>
2017-03-12 21:43:20 +0000

    Check programming done pin

--- scripts/program_nexys.tcl
+++ scripts/program_nexys.tcl
@@ -7,5 +7,15 @@ refresh_hw_device -update_hw_probes false $device
 set_property PROBES.FILE {} $device
 set_property PROGRAM.FILE {nexys_toplevel.bit} $device
 puts "\[+\] Programming device..."
+set done [get_property REGISTER.CONFIG_STATUS.BIT14_DONE_PIN [lindex [get_hw_devices xc7a100t_0] 0]]
+puts "    \[-\] DONE: $done"
 program_hw_devices $device
-exit
+set done [get_property REGISTER.CONFIG_STATUS.BIT14_DONE_PIN [lindex [get_hw_devices xc7a100t_0] 0]]
+puts "    \[-\] DONE: $done"
+if {$done == 1} {
+    puts "    \[-\] Device programmed successfully"
+    exit 0
+} else {
+    puts "    \[-\] Device programming failed"
+    exit 1
+}
