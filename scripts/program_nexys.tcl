7ff774b72e5eb672d061ca3422ecdd0d5ebe5ab2
Rich Young <rich@build.local>
2017-03-27 20:33:05 +0100

    Added commit validation to program step

--- scripts/program_nexys.tcl
+++ scripts/program_nexys.tcl
@@ -1,3 +1,4 @@
+set commit [exec git rev-parse HEAD | head -c 8]
 open_hw
 connect_hw_server
 open_hw_target
@@ -14,7 +15,14 @@ set done [get_property REGISTER.CONFIG_STATUS.BIT14_DONE_PIN [lindex [get_hw_dev
 puts "    \[-\] DONE: $done"
 if {$done == 1} {
     puts "    \[-\] Device programmed successfully"
-    exit 0
+    set usr_access [get_property REGISTER.USR_ACCESS [lindex [get_hw_devices xc7a100t_0] 0]]
+    if {$usr_access == $commit} {
+        puts "    \[-\] Bitstream USR_ACCESS value matches current commit hash $commit"
+        exit 0
+    } else {
+        puts "    \[-\] Bitstream USR_ACCESS value $usr_access does not match current commit hash $commit"
+        exit 1
+    }
 } else {
     puts "    \[-\] Device programming failed"
     exit 1
